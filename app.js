8f834bebc5885478e91169c7207bbedebccf4941
wilson <wilsongeorge1996@gmail.com>
2022-05-29 12:27:26 +0530

    Add JOI validation middleware for new and edit forms

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
+const { campgroundSchema } = require('./validationSchema');
+
 
 mongoose.connect('mongodb://localhost:27017/spot')
    .then(() => {
@@ -23,6 +25,16 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+// JOI validation middleware
+const validateCampground = (req, res, next) => {
+   const { error } = campgroundSchema.validate(req.body);
+   if (error) {
+      const msg = error.details.map(el => el.message).join(',');
+      throw new ExpressError(msg, 400);
+   } else {
+      next();
+   }
+}
 
 // Render the home page
 app.get('/', (req, res) => {
@@ -38,8 +50,8 @@ app.get('/campgrounds/new', (req, res) => {
    res.render('campgrounds/new');
 });
 // Save the new campground to database
-app.post('/campgrounds', catchAsync(async (req, res, next) => {
-   if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
+   // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
    const camp = new Campground(req.body.campground);
    await camp.save();
    res.redirect(`/campgrounds/${camp._id}`);
@@ -55,7 +67,7 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
    res.render('campgrounds/edit', { camp });
 }));
 // Update the edited campground in database
-app.put('/campgrounds/:id', catchAsync(async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
    await Campground.findByIdAndUpdate(req.params.id, req.body.campground);
    res.redirect(`/campgrounds/${req.params.id}`);
 }));
