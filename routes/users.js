e2ae0ebf12d661bae6e83c6722eac0fda791ec35
wilson <wilsongeorge1996@gmail.com>
2022-06-22 16:00:06 +0530

    Create login page, Authenticate user

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
 
+// Serve a registration form
 router.get('/register', (req, res) => {
    res.render('users/register');
 });
+// Add user to database
 router.post('/register', catchAsync(async (req, res) => {
    try {
       const { email, username, password } = req.body;
@@ -18,5 +21,28 @@ router.post('/register', catchAsync(async (req, res) => {
       res.redirect('register');
    }
 }));
+// Serve a login form
+router.get('/login', (req, res) => {
+   res.render('users/login');
+});
+// Login the user with the form data
+router.post(
+   '/login',
+   passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }),
+   (req, res) => {
+      req.flash('success', `Welcome back ${req.user.username}!`);
+      res.redirect('/campgrounds');
+   }
+);
+// Logout a user
+router.get('/logout', (req, res) => {
+   req.logout((err) => {
+      if (err)
+         return next(err);
+
+      req.flash('success', 'You are logged out!');
+      res.redirect('/campgrounds');
+   });
+});
 
 module.exports = router;
