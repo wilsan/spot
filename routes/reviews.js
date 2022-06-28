8bf158a993764d1713eea4b25e68084118578840
wilson <wilsongeorge1996@gmail.com>
2022-06-28 21:02:23 +0530

    Add star ratings

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
 const catchAsync = require('../utils/catchAsync');
-const Campground = require('../models/campground');
-const Review = require('../models/review');
 const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
 
 // Controller
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const passport = require('passport');
 const router = express.Router();
-const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
 
 // Controller
