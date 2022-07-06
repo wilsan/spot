34bf06b22e5e32d3fef62e7bbf0e648cd401305a
wilson <wilsongeorge1996@gmail.com>
2022-07-07 04:06:43 +0530

    Add custom marker to the map

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -2,10 +2,21 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
    container: 'map', // container ID
    style: 'mapbox://styles/mapbox/streets-v11', // style URL
-   center: [-74.5, 40], // starting position [lng, lat]
-   zoom: 9, // starting zoom
+   center: JSON.parse(campGeometry).coordinates, // starting position [lng, lat]
+   zoom: 11, // starting zoom
    projection: 'globe' // display the map as a 3D globe
 });
 map.on('style.load', () => {
    map.setFog({}); // Set the default atmosphere style
 });
+
+// Creating a custom marker using Bootstrap's icon
+// The 'fill' attribute sets the color
+const svg = document.createElement('div');
+svg.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16"><path fill="#f5454a" d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/></svg>'
+
+new mapboxgl.Marker({
+   element: svg
+})
+   .setLngLat(JSON.parse(campGeometry).coordinates)
+   .addTo(map);
