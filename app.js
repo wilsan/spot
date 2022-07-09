a9fe8aa6d96d7150536bb09f7bb67fe871bea6c7
wilson <wilsongeorge1996@gmail.com>
2022-07-10 03:52:43 +0530

    Sanitize mongo injections and sanitize html with joi

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const mongoSanitize = require('express-mongo-sanitize');
 
 // Routes
 const campgroundRoutes = require('./routes/campgrounds');
@@ -35,6 +36,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize());
 
 // Session & flash setup
 const sessionConfig = {
@@ -61,11 +63,10 @@ passport.deserializeUser(User.deserializeUser());
 
 // Flash message middleware
 app.use((req, res, next) => {
+   console.log(req.query);
    res.locals.success = req.flash('success');
    res.locals.error = req.flash('error');
    res.locals.currentUser = req.user;
-   // console.log(req.session);
-   // console.log(res.locals);
    next();
 });
 
