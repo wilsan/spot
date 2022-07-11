0b722ecc1930da2f7755e2e15a74845a53e76b5a
wilson <wilsongeorge1996@gmail.com>
2022-07-12 00:30:19 +0530

    Install helmet and set Content Security Policy

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
 
 // Routes
 const campgroundRoutes = require('./routes/campgrounds');
@@ -40,17 +41,68 @@ app.use(mongoSanitize());
 
 // Session & flash setup
 const sessionConfig = {
+   name: 'session',
    secret: 'thishouldbeasecret',
    resave: false,
    saveUninitialized: true,
    cookie: {
       httpOnly: true,
+      // secure: true,
       expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
       maxAge: 1000 * 60 * 60 * 24 * 7
    }
 };
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet({
+   crossOriginEmbedderPolicy: false
+}));
+
+const scriptSrcUrls = [
+   "https://stackpath.bootstrapcdn.com/",
+   "https://api.tiles.mapbox.com/",
+   "https://api.mapbox.com/",
+   "https://kit.fontawesome.com/",
+   "https://cdnjs.cloudflare.com/",
+   "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+   "https://kit-free.fontawesome.com/",
+   "https://stackpath.bootstrapcdn.com/",
+   "https://api.mapbox.com/",
+   "https://api.tiles.mapbox.com/",
+   "https://fonts.googleapis.com/",
+   "https://use.fontawesome.com/",
+   "https://cdn.jsdelivr.net/",
+];
+const connectSrcUrls = [
+   "https://api.mapbox.com/",
+   "https://a.tiles.mapbox.com/",
+   "https://b.tiles.mapbox.com/",
+   "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+   helmet.contentSecurityPolicy({
+       directives: {
+           defaultSrc: [],
+           connectSrc: ["'self'", ...connectSrcUrls],
+           scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+           styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+           workerSrc: ["'self'", "blob:"],
+           objectSrc: [],
+           imgSrc: [
+               "'self'",
+               "blob:",
+               "data:",
+               "https://res.cloudinary.com/dd15gpoln/",
+               "https://images.unsplash.com/",
+           ],
+           fontSrc: ["'self'", ...fontSrcUrls],
+       },
+   })
+);
+
 
 // Authentication
 app.use(passport.initialize());
@@ -63,7 +115,6 @@ passport.deserializeUser(User.deserializeUser());
 
 // Flash message middleware
 app.use((req, res, next) => {
-   console.log(req.query);
    res.locals.success = req.flash('success');
    res.locals.error = req.flash('error');
    res.locals.currentUser = req.user;
